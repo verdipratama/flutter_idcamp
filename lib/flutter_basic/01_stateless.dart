cb3972f66a32ef587cccd2cf26030347e8f35e97
verdipratama <verdipratama@icloud.com>
2022-07-11 15:03:44 +0700

    Initial commits

---
+++ lib/flutter_basic/01_stateless.dart
@@ -0,0 +1,15 @@
+// !! ====== README ======= !!
+// ? importM: import material design
+// ? StatelessW: StatelessWidget
+// ? Stateless Widget: Widget yang tidak dapat dirubah atau tidak akan pernah berubah.
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
