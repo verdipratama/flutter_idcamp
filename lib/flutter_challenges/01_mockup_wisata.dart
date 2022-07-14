7094e819c023709a61800c136a6fbd95c88ba9af
verdipratama <verdipratama@icloud.com>
2022-07-14 11:58:15 +0700

    chore: initial commits

---
+++ lib/flutter_challenges/01_mockup_wisata.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: slash_for_doc_comments
+// ignore_for_file: file_names
+
+/**
+ * * Flutter Tutorials: Stateful Builder
+ *
+ * ! @author Verdi Pratama <hello@verside.com>
+ * ! @link https://github.com/verdipratama/flutter_idcamp/
+ * ! @licence MIT
+ */
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Center(child: Text('IDCamp: Wisata')),
+      ),
+      // ? SafeArea() hanya mencakup bagian badannya tanpa status bar.
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                children: <Widget>[
+                  Column(),
+                  Column(),
+                  Column(),
+                ],
+              ),
+            ),
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
