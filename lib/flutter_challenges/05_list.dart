e5c1c0cae7a273f26428b7c5cbdf9704f95ae9bb
verdipratama <verdipratama@icloud.com>
2022-07-16 12:28:13 +0700

    chore: add new challenge

---
+++ lib/flutter_challenges/05_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<int> _numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title: const Text('IDCamp: ListView'),
+      ),
+      body: SafeArea(
+        child: ListView(
+          // ? Wajib Mengembalikan fungsi map menjadi objek List
+          // ? dengan fungsi .toList()
+          children: _numList.map((number) {
+            return Container(
+              height: 250.0,
+              margin: const EdgeInsets.all(16.0),
+              // width: MediaQuery.of(context).size.width,
+              // margin: const EdgeInsets.only(top: 10),
+              // color: Colors.black,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Center(
+                child: Text(
+                  number.toString(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                  ),
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_idcamp/flutter_layouts/10_listview.dart';
+import 'package:flutter_idcamp/flutter_challenges/05_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const ListBuilder(),
+      home: const Home(),
     );
   }
 }
